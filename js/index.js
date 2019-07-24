09c464b63171982c9499659d3b5a1ddbf9f91ce5
viol5229 <43625033+viol5229@users.noreply.github.com>
2019-07-24 13:28:59 -0600

    almost done

--- js/index.js
+++ js/index.js
@@ -15,39 +15,3 @@ $(window).scroll(function() {
     }
 
 });
-
-var a1 = document.getElementById("a1");
-a1.slideIndex = 1;
-showSlides(a1.slideIndex, a1);
-
-var a2 = document.getElementById("a2");
-a2.slideIndex = 1;
-showSlides(a2.slideIndex, a2);
-
-function plusSlides(n, slideshow) {
-  showSlides(slideshow.slideIndex += n, slideshow);
-}
-
-function currentSlide(n, slideshow) {
-  showSlides(slideshow.slideIndex = n, slideshow);
-}
-
-function showSlides(n, slideshow) {
-  var i;
-  var slides = slideshow.getElementsByClassName("slide");
-  var dots = slideshow.getElementsByClassName("dot");
-  if (n > slides.length){
-    slideshow.slideIndex = 1
-  }    
-  if (n < 1) {
-    slideshow.slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideshow.slideIndex-1].style.display = "block";  
-  dots[slideshow.slideIndex-1].className += " active";
-}
