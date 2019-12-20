212b012e6138520a16b289923fcea13dbfd114fb
viol5229 <43625033+viol5229@users.noreply.github.com>
2019-12-19 16:30:21 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 var position = $(window).scrollTop(); 
 
-// should start at 0
-if(document.title = 'index.html'){
+document.getElementById('h1').className = "h1OG";
+document.getElementById('nav1').className = "nav1OG";
+
+// should start at 0, but does not when new page is open
 	$(window).scroll(function() {
 	    var scroll = $(window).scrollTop();
 	    if(scroll > 200){
@@ -15,4 +17,17 @@ if(document.title = 'index.html'){
 	    }
 
 	});
-}
+
+var old = 1;
+var next = 3;
+$("#next").click(function(){
+	$("#slide"+old).css("display", "none");
+	old++;
+	$("#slide"+next).css("display", "inline-block");
+	next++;
+	if (next == 12) {
+		next = 1;
+	}else if (old == 12){
+		old = 1;
+	}
+});
